0e628c6281c5a2f58704718b114777f51ab0f659
PavanKalyan <pavankalyan6md@gmail.com>
2023-11-02 12:06:18 +0530

    commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(conversion_multiple,id, currency_from, currency_to,environment)
+values(80,1001,'USD','INR','');
+insert into currency_exchange(conversion_multiple,id, currency_from, currency_to,environment)
+values(75,1002,'AUD','INR','');
+insert into currency_exchange(conversion_multiple,id, currency_from, currency_to,environment)
+values(65,1003,'EUR','INR','');
